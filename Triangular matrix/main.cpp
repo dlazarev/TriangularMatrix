456fc831094a9852b33f188cd1cf5cdcb68c376e
Dmitry Lazarev <diman.lazarev@gmail.com>
2012-11-13 11:57:50 +0400

    Initial Commit

---
+++ Triangular matrix/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Triangular matrix
+//
+//  Created by Dmitry Lazarev on 13.11.12.
+//  Copyright (c) 2012 Ilya Lazarev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
